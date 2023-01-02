2cc0de6f5301297740fac41b6379decf6211593d
COOL- <87934102+msajo@users.noreply.github.com>
2023-01-02 17:10:55 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -16,3 +16,24 @@ function take_snapshot(){
 console.log("ml5 version",ml5.version);
 
 classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/aJRhvtMay7/model.json",modelLoaded);
+
+function modelLoaded(){
+    console.log("model loaded");
+}
+
+function check(){
+    img = document.getElementById("capture_image");
+    classifier.classify(img,gotresult);
+}
+
+function gotresult(error,results){
+    if(error){
+        console.log("error");
+    }
+    else{
+        console.log("results");
+        document.getElementById("object_name").innerHTML = results[0].label;
+        document.getElementById("object_accuracy").innerHTML = results[0].confidence.toFixed(3)
+
+    }
+}
